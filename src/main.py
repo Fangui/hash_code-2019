28ec75b82d3e48ee2d07803de1002753de07eaa1
Brendan Harley <brendan@lse.epita.fr>
2019-02-28 19:40:30 +0100

    patch python main
    Signed-off-by: Brendan Harley <brendan@lse.epita.fr>

--- src/main.py
+++ src/main.py
@@ -1,30 +1,30 @@
+#!/usr/bin/env python3
 import sys
 
-def parse():
-        filename = sys.argv[1]
+def parse(filename):
         with open(filename) as f:
                 size = f.readline()
                 for data in f:
                         raw.append(data.split( ))
         return size, raw
 
-res = []
 
-s_size = 0
-
-def dump():
+def dump(res, s_size):
         print(s_size)
         for data in res:
                 print(data)
         return;
 
 
+res = []
+s_size = 0
 
 size = 0
 raw = []
 
-(size, raw) = parse()
+filename = sys.argv[1]
+(size, raw) = parse(filename)
 s_size = size[0]
 for i in range(0, int(size)):
         res.append(i)
-dump()
+dump(res, s_size)
